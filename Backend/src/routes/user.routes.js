2aa4eba69aad9797978485ece7c3c87784831fa5
OnShubham <shubhamkharche3005@gmail.com>
2024-09-17 10:23:02 +0530

    m

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -4,6 +4,13 @@ import {
   UserLogin,
   logout,
   refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountHandler,
+  updateUserAvatar,
+  updateCoverImages,
+  getUserChanaels,
+  getWatchHistory,
 } from '../controllers/user.controller.js';
 import { upload } from '../middlewares/multer,middleware.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
@@ -28,4 +35,20 @@ router.route('/register').post(
 router.route('/login').post(UserLogin);
 router.route('/logout').post(verifyJWT, logout);
 router.route('/refresh-token').post(refreshAccessToken);
+router.route('/forget-pasword').post(verifyJWT, changeCurrentPassword),
+  router.route('/curent-user').get(verifyJWT, getCurrentUser),
+  router
+    .route('/update-account-details')
+    .patch(verifyJWT, updateAccountHandler),
+  router
+    .route('/update-avatar')
+    .patch(verifyJWT, upload.single('avatar'), updateUserAvatar);
+
+router
+  .route('/cover-image')
+  .patch(verifyJWT, upload.single('coverImage'), updateCoverImages);
+
+router.route('/c/:username').get(verifyJWT, getUserChanaels);
+router.route('/watchHistory').get(verifyJWT, getWatchHistory);
+
 export default router;
